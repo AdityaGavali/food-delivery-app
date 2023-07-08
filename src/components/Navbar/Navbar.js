7221b2d2800d4f79df08b7bd4c4f8d8d42b1ccf5
AdityaGavali <adityagavali18@gmail.com>
2023-07-08 16:24:04 +0530

    navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Navbar.css"
+function Navbar() {
+  return (
+    <div className='navbar-container'>
+    {/* logo */}
+    <div>
+    
+    <img className='navbar-logo'src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="" />
+    </div>
+    {/* search bar */}
+    <div className='navbar-search'>
+    search
+    </div>
+    
+    </div>
+  )
+}
+
+export default Navbar
